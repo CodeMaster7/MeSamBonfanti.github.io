ae84335c68a323e9248a41a7ad79364019b41e2f
Sam Bonfanti <Sam Bonfanti>
2016-12-24 07:12:33 -0700

    got rid off some javascript that i was not using

--- assets/javascript/custom.js
+++ assets/javascript/custom.js
@@ -196,39 +196,39 @@ $(document).ready(function() {
 });
 
 
-$(window).load(function(){ 
-
-  // -------------- Jquery Isotope Setting -------------- 
-
-  var $container = $('.portfoliocontent');
-  $container.isotope({
-    filter: '*',
-    animationOptions: {
-      duration: 750,
-      easing: 'linear',
-      queue: false
-    }
-  });
-
-  $('.portfoliofilter a').on('click', function(e) {
-    e.preventDefault();
-    $('.portfoliofilter .current').removeClass('current');
-    $(this).addClass('current');
-
-    var selector = $(this).attr('data-filter');
-    $container.isotope({
-      filter: selector,
-      animationOptions: {
-        duration: 750,
-        easing: 'linear',
-        queue: false
-      }
-     });
-     return false;
-  });
-
-  // -------------- Product Showcase -------------- 
-
-  $('.sp-wrap').smoothproducts();
-
-});
+//$(window).load(function(){ 
+//
+//  // -------------- Jquery Isotope Setting -------------- 
+//
+//  var $container = $('.portfoliocontent');
+//  $container.isotope({
+//    filter: '*',
+//    animationOptions: {
+//      duration: 750,
+//      easing: 'linear',
+//      queue: false
+//    }
+//  });
+//
+//  $('.portfoliofilter a').on('click', function(e) {
+//    e.preventDefault();
+//    $('.portfoliofilter .current').removeClass('current');
+//    $(this).addClass('current');
+//
+//    var selector = $(this).attr('data-filter');
+//    $container.isotope({
+//      filter: selector,
+//      animationOptions: {
+//        duration: 750,
+//        easing: 'linear',
+//        queue: false
+//      }
+//     });
+//     return false;
+//  });
+//
+//  // -------------- Product Showcase -------------- 
+//
+//  $('.sp-wrap').smoothproducts();
+//
+//});
